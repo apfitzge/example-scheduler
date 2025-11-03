d04f494bf9319773af3f4bb8393d60d0de49440a
Andrew Fitzgerald <apfitzge@gmail.com>
2025-11-03 12:19:28 -0600

    ctrl-c signal-hook for graceful exit

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use std::sync::{
+    Arc,
+    atomic::{AtomicBool, Ordering},
+};
+
+fn main() {
+    let exit = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGINT, exit.clone())
+        .expect("failed to register signal handler");
+
+    while !exit.load(Ordering::Relaxed) {}
+}
